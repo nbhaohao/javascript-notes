fd04c84a5d6a6e33fed062407e1263fce3748162
zhangzh <zhangzh@cnlemon.net>
2019-10-04 14:47:21 +0800

    feat: 完善 bind 代码，让函数可以被 new 以及保持原型正确

--- code/bind-polyfill/bind-polyfill.test.ts
+++ code/bind-polyfill/bind-polyfill.test.ts
@@ -9,13 +9,13 @@ declare global {
 
 describe("测试 bind-polyfill", () => {
   beforeAll(() => {
-    // Function.prototype.bind2 = bindPolyfill_es6;
-    Function.prototype.bind2 = bindPolyfill;
+    Function.prototype.bind2 = bindPolyfill_es6;
+    // Function.prototype.bind2 = bindPolyfill;
   });
   it("普通函数上存在 bindPolyFill", () => {
     const fn = () => {};
-    // expect(fn.bind2).toBe(bindPolyfill_es6);
-    expect(fn.bind2).toBe(bindPolyfill);
+    expect(fn.bind2).toBe(bindPolyfill_es6);
+    // expect(fn.bind2).toBe(bindPolyfill);
   });
   it("可以将第一个参数作为新函数调用的 this", () => {
     const test_fn = function(this: any): any {
@@ -40,5 +40,40 @@ describe("测试 bind-polyfill", () => {
     expect(new_test_fn1(2)).toEqual([undefined, "1", 2]);
     const new_test_fn2 = test_fn1.bind2(undefined);
     expect(new_test_fn2(1, 2)).toEqual([undefined, 1, 2]);
+  });
+  it("可以 new ", () => {
+    const test_fn1 = function(this: any, a: any, b: any) {
+      this.a = a;
+      this.b = b;
+    };
+    const new_fn1 = test_fn1.bind2(undefined, "x", "y");
+    const fn1_new_result = { a: "x", b: "y" };
+    // @ts-ignore
+    expect(new new_fn1()).toEqual(fn1_new_result);
+  });
+  it("测试改变了函数的 prototype, 再用 new 的情况", () => {
+    const test_fn1 = function(this: any, a: any, b: any) {
+      this.a = a;
+      this.b = b;
+    };
+    test_fn1.prototype.sayHi = function() {};
+    const new_fn1 = test_fn1.bind2(undefined, "x", "y");
+    const fn1_new_result = { a: "x", b: "y" };
+    // @ts-ignore
+    const new_fn1_result = new new_fn1();
+    expect(new_fn1_result).toEqual(fn1_new_result);
+    expect(new_fn1_result.sayHi).not.toBeUndefined();
+  });
+  it("测试比较极端的 bind this 的情况，会导致无法判断是否使用了 new", () => {
+    const test_fn1 = function(this: any, a: any, b: any) {
+      this.a = a;
+      this.b = b;
+    };
+    // @ts-ignore
+    const test_temp_obj = new test_fn1("5", "6");
+    const new_fn1 = test_fn1.bind2(test_temp_obj, "3", "4");
+    new_fn1();
+    expect(test_temp_obj.a).toBe("3");
+    expect(test_temp_obj.b).toBe("4");
   });
 });
--- code/bind-polyfill/bind-polyfill.ts
+++ code/bind-polyfill/bind-polyfill.ts
@@ -3,22 +3,32 @@ function bindPolyfill(this: Function, thisArg: any): any {
   var fn = this;
   var slice = Array.prototype.slice;
   var args = slice.call(arguments, 1);
-  return function() {
+  function temp(this: any) {
     var newArgs = slice.call(arguments, 0);
-    return fn.apply(thisArg, args.concat(newArgs));
-  };
+    return fn.apply(
+      this instanceof temp ? this : thisArg,
+      args.concat(newArgs)
+    );
+  }
+  temp.prototype = fn.prototype;
+  return temp;
 }
 
 // 使用 es6 语法更方便的写法
+// this instanceof temp 是为了让我们返回的函数可以被 new.
+// 因为 new 操作符会传入一个临时对象作为 this, 在这种情况下，我们应该使用原本的 this
+// 那为了判断当前函数是否是通过 new 操作符调用的，我们就看当前 this instanceof temp.
 function bindPolyfill_es6(
   this: Function,
   thisArg: any,
   ...argArray: any[]
 ): any {
   const fn = this;
-  return function(this: any, ...args: Array<any>): any {
-    return fn.call(thisArg, ...argArray, ...args);
-  };
+  function temp(this: any, ...args: Array<any>): any {
+    return fn.call(this instanceof temp ? this : thisArg, ...argArray, ...args);
+  }
+  temp.prototype = fn.prototype;
+  return temp;
 }
 
 if (!Function.prototype.bind) {
